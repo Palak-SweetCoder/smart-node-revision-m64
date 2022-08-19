540df2bf095dcb7e4a929acf53646a6bf8c6b67f
Palak-SweetCoder <pspalak6@gmail.com>
2022-08-19 13:07:04 +0600

    server side setup completed

---
+++ __steps.js
@@ -0,0 +1,44 @@
+/**
+ * -----------------------------
+ * -----SETUP SERVER SIDE-------
+ * -----------------------------
+ * Go to expressjs.com >> getting started >> installing
+ * CMD go to projects >> run this command >> mkdir appname
+ * Go to app folder run this command >> npm init or npm init -y (to avoid multiple questions)
+ * Run this command >> npm install express
+ * Create index.js file
+ * Server side setup COMPLETED!!!
+ * 
+ * -----------------------------
+ * -----SETUP NODE EXPRESS------
+ * -----------------------------
+ * Go to expressjs.com >> getting started >> hello world
+ * Import or make require the express
+ * Declare app
+ * Declare the port
+ * Set app.get function
+ * Set app.listen function
+ * Run this command >> node index.js
+ * Go to localhost:5000
+ * Install nodemon run this command >> npm install -g nodemon
+ * Go to package.json >> scripts >> add this two line scripts >> "start": "node index.js", "start-dev" : "nodemon index.js",
+ * Run this command to open the port >> nodemon index.js
+ * Node express setup COMPLETED!!!
+ * 
+ * -----------------------------
+ * -----SETUP SERVER SIDE-------
+ * -----------------------------
+ * 
+ * 
+ * 
+ * -----------------------------
+ * -----SETUP SERVER SIDE-------
+ * -----------------------------
+ * 
+ * 
+ * 
+ * 
+ * -----------------------------
+ * -----SETUP SERVER SIDE-------
+ * -----------------------------
+ */
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+    res.send('Hey Dude!! I am okay.')
+})
+
+const users = [
+    { id: 1, name: 'sabana', email: 'sabana@gmail.com', phone: '01788888888' },
+    { id: 2, name: 'sabnur', email: 'sabnur@gmail.com', phone: '01788888888' },
+    { id: 3, name: 'purnima', email: 'purnima@gmail.com', phone: '01788888888' },
+    { id: 4, name: 'jin', email: 'jin@gmail.com', phone: '01788888888' },
+    { id: 5, name: 'vut', email: 'vut@gmail.com', phone: '01788888888' },
+    { id: 6, name: 'petni', email: 'petni@gmail.com', phone: '01788888888' },
+    { id: 7, name: 'pori', email: 'pori@gmail.com', phone: '01788888888' },
+]
+
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+
+app.get('/user/:id', (req, res) => {
+    console.log(req.params);
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id === id);
+    res.send(user); 
+})
+
+app.listen(port, () => {
+    console.log("Port running don't worry", port)
+})
